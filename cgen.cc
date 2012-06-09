857e3c51b6145de435cb6054314b09b5a61807df
Grant Ding Ho <grantho@corn20.stanford.edu>
2012-06-09 15:32:58 -0700

    Fixed nested-let : PRoblem with neg_class codegen

--- cgen.cc
+++ cgen.cc
@@ -1818,7 +1818,8 @@ void neg_class::code(ostream &s, CgenClassTable *ctable, CgenNodeP curClass) {
   e1->code(s, ctable, curClass);
   emit_fetch_int(T1, ACC, s);
   emit_neg(T1, T1, s);
-  emit_store_int(T1, ACC, s);
+  emit_jal("Object.copy", s);
+  emit_store(T1, DEFAULT_OBJFIELDS, ACC, s);
 }
 
 void lt_class::code(ostream &s, CgenClassTable *ctable, CgenNodeP curClass) {
